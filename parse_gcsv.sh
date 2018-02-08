5b19d23a64723c491af63b7a359c5d8ba860a60b
v <valka@cyberpunk.party>
2018-02-08 16:01:08 -0500

    clean up formatting

--- parse_gcsv.sh
+++ parse_gcsv.sh
@@ -1,55 +1,63 @@
 #!/bin/bash
-wget 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTcIujSZ6oIwHvRJ87Bg0S-vVBNCTYfy3MEjcD-2Apn0Y2Bu2oDxg2NcgA0IfNh6YbsyZfFh7uf9pZN/pub?gid=1392688163&single=true&output=csv' -O todaysharvest
+
+doc_url='https://docs.google.com/spreadsheets/d/e/2PACX-1vTcIujSZ6oIwHvRJ87Bg0S-vVBNCTYfy3MEjcD-2Apn0Y2Bu2oDxg2NcgA0IfNh6YbsyZfFh7uf9pZN/pub?gid=1392688163&single=true&output=csv'
+wget "$url" -O todaysharvest
 
 {
 read
 while IFS='' read -r line || [[ -n "$line" ]]; do
 
-crop="$(echo "$line" | cut -d ',' -f 2 | tr -d '[:space:]')"
-harvest_date="$(echo "$line" | cut -d ',' -f 3| tr -d '[:space:]')"
-date="$(date -d $harvest_date +%s%N)"
-weight="$(echo "$line" | cut -d ',' -f 4 | tr -d '[:space:]')"
-piece="$(echo "$line" | cut -d ',' -f 5 | tr -d '[:space:]')"
-location="$(echo "$line" | cut -d ',' -f 6 | tr -d '[:space:]')"
+  crop="$(echo "$line" | cut -d ',' -f 2 | tr -d '[:space:]')"
+  harvest_date="$(echo "$line" | cut -d ',' -f 3| tr -d '[:space:]')"
+  date="$(date -d $harvest_date +%s%N)"
+  weight="$(echo "$line" | cut -d ',' -f 4 | tr -d '[:space:]')"
+  piece="$(echo "$line" | cut -d ',' -f 5 | tr -d '[:space:]')"
+  location="$(echo "$line" | cut -d ',' -f 6 | tr -d '[:space:]')"
 
-if [[ ! -z "${crop// }" ]]
-then 
-	echo -n "$crop "
-else
-       	echo -n "none "
-	crop="undefined_crop"
-fi
-if [[ ! -z "${weight// }" ]]
-then
-        echo -n "$weight "
-else
-	echo -n "none "
-	weight="0"
-fi
-if [[ ! -z "${piece// }" ]]
-	then
-echo -n "$piece "
-else
-	echo -n "none "
-	piece="0"
-fi
-if [[ ! -z "${location// }" ]]
-then
-echo -n "$location "
-else
-       	echo -n "none "
-	location="none"
-fi
-if [[ ! -z "${date// }" ]]
-then 
-	echo "$date"
-else
-	echo "none"
-	date="invalid"
-fi
-lower_ensured=$(echo "$crop,location=$location weight=$weight,pieces=$piece $date" | tr '[:upper:]' '[:lower:]')
-echo $lower_ensured
-curl -i -XPOST 'http://localhost:8086/write?db=collectd' --data-binary "$lower_ensured"
-sleep 5
+  if [[ ! -z "${crop// }" ]]
+    then 
+    echo -n "$crop "
+  else
+    echo -n "none "
+    crop="undefined_crop"
+  fi
+
+  if [[ ! -z "${weight// }" ]]
+    then
+    echo -n "$weight "
+  else
+    echo -n "none "
+      weight="0"
+  fi
+
+  if [[ ! -z "${piece// }" ]]
+    then
+    echo -n "$piece "
+  else
+    echo -n "none "
+    piece="0"
+  fi
+
+  if [[ ! -z "${location// }" ]]
+    then
+    echo -n "$location "
+  else
+    echo -n "none "
+    location="none"
+  fi
+
+  if [[ ! -z "${date// }" ]]
+    then 
+    echo "$date"
+  else
+    echo "none"
+    date="invalid"
+  fi
+
+  lower_ensured=$(echo "$crop,location=$location weight=$weight,pieces=$piece $date" | tr '[:upper:]' '[:lower:]')
+  echo $lower_ensured
+  curl -i -XPOST 'http://localhost:8086/write?db=collectd' --data-binary "$lower_ensured"
+  sleep 5
+
 done
 } < "todaysharvest"
